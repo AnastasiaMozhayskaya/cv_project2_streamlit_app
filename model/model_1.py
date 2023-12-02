be42c894c3326b08318135c0fd624ca3976a2a05
Анастасия <Anastasia@MacBook-Pro-Anastasia.local>
2023-12-02 22:16:42 +0300

    y5

--- model/model_1.py
+++ model/model_1.py
@@ -1,7 +1,10 @@
 import torch
 from model.preprocessing import make_transform
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/bestweights_yolov5l.pt', force_reload=True)
+# model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/bestweights_yolov5l.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 
+                       'custom', 
+                       path='weights/bestweights_yolov5l.pt')
 # model = torch.hub.load('yolov5', 'custom', path='weights/bestweights_yolov5l.pt', source='local')
 
 def predict_1(image):
